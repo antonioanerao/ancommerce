87bc3cb44a13f8d75999e343ad6853324260e2ba
Antonio Anerão <anerao.junior@gmail.com>
2020-06-29 17:21:43 -0500

    Adding customizer and footer widgets

--- footer.php
+++ footer.php
@@ -23,43 +23,42 @@
 <footer class="footer bg-dark">
     <div class="container">
         <div class="row">
-            <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
-                <ul class="list-inline mb-2">
-                    <li class="list-inline-item">
-                        <a href="#" class="text-white text-decoration-none" >About</a>
-                    </li>
-                    <li class="list-inline-item">&sdot;</li>
-                    <li class="list-inline-item">
-                        <a class="text-white text-decoration-none" href="#">Contact</a>
-                    </li>
-                    <li class="list-inline-item">&sdot;</li>
-                    <li class="list-inline-item">
-                        <a class="text-white text-decoration-none" href="#">Terms of Use</a>
-                    </li>
-                    <li class="list-inline-item">&sdot;</li>
-                    <li class="list-inline-item">
-                        <a class="text-white text-decoration-none" href="#">Privacy Policy</a>
-                    </li>
-                </ul>
-                <p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2020. All Rights Reserved.</p>
+            <div class="footer-menu col-lg-6 h-100 text-center text-lg-left my-auto">
+                <?php
+                wp_nav_menu(
+                    array(
+                        'theme_location'	=> 'ancommerce_footer_menu',
+                        'depth'				=> 1,
+                    )
+                );
+                ?>
+                <p class="text-muted small mb-4 mb-lg-0"><br> <?php echo esc_html( get_theme_mod( 'set_copyright', __( 'Copyright X - All Rights Reserved', 'ancommerce' ) ) ); ?></p>
             </div>
             <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
                 <ul class="list-inline mb-0">
-                    <li class="list-inline-item mr-3">
-                        <a href="#">
-                            <i class="text-white fab fa-facebook fa-2x fa-fw"></i>
-                        </a>
-                    </li>
-                    <li class="list-inline-item mr-3">
-                        <a href="#">
-                            <i class="text-white  fab fa-twitter-square fa-2x fa-fw"></i>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="#">
-                            <i class="text-white fab fa-instagram fa-2x fa-fw"></i>
-                        </a>
-                    </li>
+                    <?php if(get_theme_mod( 'set_social_instagram')) : ?>
+                        <li class="list-inline-item">
+                            <a href="https://www.instagram.com/<?php echo get_theme_mod( 'set_social_instagram'); ?>" target="_blank">
+                                <i class="text-white fab fa-instagram fa-2x fa-fw"></i>
+                            </a>
+                        </li>
+                    <?php endif; ?>
+
+                    <?php if(get_theme_mod( 'set_social_pinterest')) : ?>
+                        <li class="list-inline-item">
+                            <a href="https://www.pinterest.com/<?php echo get_theme_mod( 'set_social_pinterest'); ?>" target="_blank">
+                                <i class="text-white fab fa-pinterest fa-2x fa-fw"></i>
+                            </a>
+                        </li>
+                    <?php endif; ?>
+
+                    <?php if(get_theme_mod( 'set_social_facebook')) : ?>
+                        <li class="list-inline-item">
+                            <a href="https://www.facebook.com/<?php echo get_theme_mod( 'set_social_facebook'); ?>" target="_blank">
+                                <i class="text-white fab fa-facebook fa-2x fa-fw"></i>
+                            </a>
+                        </li>
+                    <?php endif; ?>
                 </ul>
             </div>
         </div>
--- inc/customizer.php
+++ inc/customizer.php
@@ -210,6 +210,107 @@ function ancommerce_customizer($wp_customize) {
         )
     );
 
+
+    // Footer
+
+    //Panel Footer Settings
+    $wp_customize->add_panel( 'panel_footer_settings', array(
+        'priority'       => 500,
+        'capability'     => 'edit_theme_options',
+        'title'      => __('Footer Settings', 'ancommerce'),
+    ) );
+
+        /* Copyright section start */
+        $wp_customize->add_section(
+            'sec_copyright', array(
+                'title'			=> __( 'Copyright Settings', 'ancommerce' ),
+                'description'	=> __( 'Copyright Section', 'ancommerce' ),
+                'panel'  => 'panel_footer_settings',
+            )
+        );
+            // Field 1 - Copyright Text Box
+            $wp_customize->add_setting(
+                'set_copyright', array(
+                    'type'					=> 'theme_mod',
+                    'default'				=> '',
+                    'sanitize_callback'		=> 'sanitize_text_field',
+                )
+            );
+
+            $wp_customize->add_control(
+                'set_copyright', array(
+                    'label'				=> __( 'Copyright', 'ancommerce' ),
+                    'description'		=> __( 'Please, add your copyright information here', 'ancommerce' ),
+                    'section'			=> 'sec_copyright',
+                    'type'				=> 'text'
+                )
+            );
+            /* Copyright section ends */
+
+
+
+    /* Social Media section start */
+    $wp_customize->add_section(
+        'sec_social', array(
+            'title'			=> __( 'Social Media Settings', 'ancommerce' ),
+            'description'	=> __( 'Social Media Section', 'ancommerce' ),
+            'panel'  => 'panel_footer_settings',
+    ));
+
+        // Instagram
+        $wp_customize->add_setting(
+            'set_social_instagram', array(
+                'type'					=> 'theme_mod',
+                'default'				=> '',
+                'sanitize_callback'		=> 'sanitize_text_field',
+            )
+        );
+
+        $wp_customize->add_control(
+            'set_social_instagram', array(
+            'label'				=> __( 'Instagram URL', 'ancommerce' ),
+            'description'		=> __( 'Please, add your full Instagram url here', 'ancommerce' ),
+            'section'			=> 'sec_social',
+            'type'				=> 'text'
+        ));
+
+        // Pinterest
+        $wp_customize->add_setting(
+            'set_social_pinterest', array(
+                'type'					=> 'theme_mod',
+                'default'				=> '',
+                'sanitize_callback'		=> 'sanitize_text_field',
+            )
+        );
+
+        $wp_customize->add_control(
+            'set_social_pinterest', array(
+            'label'				=> __( 'Pinterest URL', 'ancommerce' ),
+            'description'		=> __( 'Please, add your full Pinterest url here', 'ancommerce' ),
+            'section'			=> 'sec_social',
+            'type'				=> 'text'
+        ));
+
+        // Facebook
+        $wp_customize->add_setting(
+            'set_social_facebook', array(
+                'type'					=> 'theme_mod',
+                'default'				=> '',
+                'sanitize_callback'		=> 'sanitize_text_field',
+            )
+        );
+
+        $wp_customize->add_control(
+            'set_social_facebook', array(
+                'label'				=> __( 'Facebook URL', 'ancommerce' ),
+                'description'		=> __( 'Please, add your full Facebook url here', 'ancommerce' ),
+                'section'			=> 'sec_social',
+                'type'				=> 'text'
+            ));
+
+    /* Social Media section ends */
+
+    //Sanitize html
     function appointment_sanitize_html( $input )
     {
         return force_balance_tags( $input );

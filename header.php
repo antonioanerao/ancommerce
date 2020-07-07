c10091afc739f25a55114d661295475d6cdf2ca6
Antonio Anerão <anerao.junior@gmail.com>
2020-07-07 11:59:23 -0500

    Adicionado topBar

--- header.php
+++ header.php
@@ -11,7 +11,66 @@
     if(get_theme_mod( 'set_theme_color') == 'darkorange') { $themeNavbar = "navbar-dark bg-darkorange"; $themeColor = "theme-darkorange"; }
 ?>
 <body <?php body_class($themeColor); ?>>
+<div class="topBar">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 d-none d-md-block">
+                <ul class="list-inline mb-0">
+                    <?php if(get_theme_mod( 'set_social_instagram')) : ?>
+                        <li class="list-inline-item">
+                            <a href="https://www.instagram.com/<?php echo get_theme_mod( 'set_social_instagram'); ?>" target="_blank">
+                                <i class="text-white fab fa-instagram social-icons"></i>
+                            </a>
+                        </li>
+                    <?php endif; ?>
 
+                    <?php if(get_theme_mod( 'set_social_pinterest')) : ?>
+                        <li class="list-inline-item">
+                            <a href="https://www.pinterest.com/<?php echo get_theme_mod( 'set_social_pinterest'); ?>" target="_blank">
+                                <i class="text-white fab fa-pinterest social-icons"></i>
+                            </a>
+                        </li>
+                    <?php endif; ?>
+
+                    <?php if(get_theme_mod( 'set_social_facebook')) : ?>
+                        <li class="list-inline-item">
+                            <a href="https://www.facebook.com/<?php echo get_theme_mod( 'set_social_facebook'); ?>" target="_blank">
+                                <i class="text-white fab fa-facebook social-icons"></i>
+                            </a>
+                        </li>
+                    <?php endif; ?>
+                </ul>
+            </div>
+            <div class="col-md-6 col-12">
+                <ul class="list-inline float-right top-right">
+                    <?php if ( is_user_logged_in() ) : ?>
+                        <li class="account-login">
+                            <span>
+                                <a href="<?php echo esc_url( get_permalink( get_option('woocommerce_myaccount_page_id') ) ); ?>"><?php esc_html_e( 'My Account', 'ancommerce' ) ?></a>
+                                <small>/</small>
+                                <a href="<?php echo esc_url( wp_logout_url( get_permalink( get_option('woocommerce_myaccount_page_id') ) ) );?>"><?php esc_html_e( 'Logout', 'ancommerce'); ?></a>
+                            </span>
+                        </li>
+                    <?php else: ?>
+                        <li class="account-login">
+                        <span>
+                            <a class="account-link" href="<?php echo esc_url( get_permalink( get_option('woocommerce_myaccount_page_id') ) ); ?>"><?php esc_html_e( 'Login / Register', 'ancommerce' ) ?></a>
+                        </span>
+                        </li>
+                    <?php endif; ?>
+
+                    <li class="cart-topbar">
+                        <a href="<?php echo wc_get_cart_url(); ?>"><i class="fa fa-shopping-cart"></i>
+                            <span class="items">
+                                <?php echo WC()->cart->get_cart_contents_count(); ?>
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
 <nav class="navbar navbar-expand-lg sb-navbar <?php echo $themeNavbar; ?> ">
     <div class="container">
 
@@ -41,15 +100,5 @@
             ) );
         ?>
 
-        <?php if( class_exists( 'WooCommerce' ) ): ?>
-            <div class="cart-header d-block mx-auto mx-md-0 mx-sm-0">
-                <a href="<?php echo wc_get_cart_url(); ?>">
-                    <span class="fa fa-shopping-cart"></span> <?php _e('My Cart', 'ancommerce') ?>
-                </a>
-                <span class="items">
-                    <?php echo WC()->cart->get_cart_contents_count(); ?>
-                </span>
-            </div>
-        <?php endif; ?>
     </div>
 </nav>
